f81db3d9ddbb4ccdac32e52da0211b7318de8172
nugraha <mangcoding@gmail.com>
2017-01-25 11:23:52 +0700

    update looping

--- looping_1.cpp
+++ looping_1.cpp
@@ -4,7 +4,7 @@ int main() {
 	int a,b;	
 	for (a=1; a<=6; a++) {
 		for (b=a; b<=a+5; b++) {
-			if (a==3 && (b ==a+2 || b==a+3) || a==4 && (b ==a+2 || b==a+3)) printf("- ");
+			if ((a==3  || a==4) && (b ==a+2 || b==a+3)) printf("- ");
 			else if (b%2==1) printf("0 ");
 			else printf("X ");
 		}
